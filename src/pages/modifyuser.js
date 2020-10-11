ff8df90ec0ccebc9f32e283ed129201fdc14c13f
sbetsabe <bet.rlopez@gmail.com>
2020-10-11 17:53:40 -0600

    Formularios

--- src/pages/modifyuser.js
+++ src/pages/modifyuser.js
@@ -1,6 +1,25 @@
 import React from 'react';
 export default () => (
-    <div>
-        Modificar Usuario
+    <div className="container">
+      <h2>Modificar usuario</h2>
+      <form className="was-validated">
+        <div className="form-group">
+          <label>Cedula:</label>
+          <input type="text" className="form-control" placeholder="Escriba su cedula" required/>
+        </div>
+        <div className="form-group">
+          <label>Nombre:</label>
+          <input type="text" className="form-control" placeholder="Escriba su nombre" required/>
+        </div>
+        <div className="form-group">
+          <label>Apellidos:</label>
+          <input type="text" className="form-control" placeholder="Escriba sus apellidos" required/>
+        </div>
+        <div className="form-group">
+          <label>Correo electronico:</label>
+          <input type="email" className="form-control" placeholder="Escriba su correo electronico" required/>
+        </div>
+        <button type="submit" class="btn btn-primary">Guardar</button>
+      </form>
     </div>
 );
--- src/pages/newuser.js
+++ src/pages/newuser.js
@@ -1,6 +1,25 @@
 import React from 'react';
 export default () => (
-    <div>
-        Nuevo Usuario
+    <div className="container">
+      <h2>Ingresar nuevo usuario</h2>
+      <form className="was-validated">
+        <div className="form-group">
+          <label>Cedula:</label>
+          <input type="text" className="form-control" placeholder="Escriba su cedula" required/>
+        </div>
+        <div className="form-group">
+          <label>Nombre:</label>
+          <input type="text" className="form-control" placeholder="Escriba su nombre" required/>
+        </div>
+        <div className="form-group">
+          <label>Apellidos:</label>
+          <input type="text" className="form-control" placeholder="Escriba sus apellidos" required/>
+        </div>
+        <div className="form-group">
+          <label>Correo electronico:</label>
+          <input type="email" className="form-control" placeholder="Escriba su correo electronico" required/>
+        </div>
+        <button type="submit" class="btn btn-primary">Guardar</button>
+      </form>
     </div>
 );
