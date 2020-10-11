30ed046dd439212ce98d9509cb5b5390a96abbfa
sbetsabe <bet.rlopez@gmail.com>
2020-10-10 19:18:17 -0600

    Jumbo tron

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/js/bootstrap.bundle'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +13,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends React.Component {
     }
     render() {
         return (
-            <nav className="navbar navbar-inverse" >
+          <nav className="navbar navbar-inverse bg-dark" >
                 <div className="container-fluid">
                     <div className="navbar-header">
                         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
